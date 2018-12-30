76ab71c3f164365f127c6edd6ac1c187d8e4b1bc
gauravpathak <gauravpathak.spring@gmail.com>
2018-12-30 22:06:09 +0530

    Initial commit

---
+++ src/main/java/com/gaurav/api/RestfulFaketoryApplication.java
@@ -0,0 +1,14 @@
+package com.gaurav.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulFaketoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulFaketoryApplication.class, args);
+    }
+
+}
+
